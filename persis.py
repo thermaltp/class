b1e1bed41bf05ea93a37c4525d2c12e28ca93fef
thermaltp <thermal@gmail.com>
2018-06-12 08:51:11 -0400

    Create persis.py

---
+++ persis.py
@@ -0,0 +1,28 @@
+import os
+import time
+exist = "persis.py"
+def birth():
+    list = os.popen("ls").read()
+    if exist in list:
+        print("Script exists")
+    else:
+        os.system("cd ~/Desktop")
+    #os.system("wget URL")
+def working():
+    running = os.popen("ps -elf").read()
+    if exist in running:
+        print("ye were running")
+        time.sleep(60)
+    else:
+        print("We starting up")
+        os.system("python persis.py")
+def persistace():
+    os.system("crontab -l > mycron")
+    os.system("echo '* * * * * /usr/bin/python /home/usacys/persis.py' >> mycron")
+    os.system("crontab mycron")
+    os.system("rm mycron")
+
+persistace()
+while True:
+    birth()
+    working()
